75e405c04ffc27454e310a17c53619c9ca0118ce
Shubham Shaw <shubhamshaw022@gmail.com>
2021-04-19 00:18:54 +0530

    123

--- Admin/add_item.php
+++ Admin/add_item.php
@@ -9,31 +9,18 @@ $con = mysqli_connect($DATABASE_HOST,$DATABASE_USER,$DATABASE_PASS,$DATABASE_NAM
 if (mysqli_connect_errno() ) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-if ($stmt = $con->prepare('SELECT * FROM meds WHERE id=?')) {
-    $stmt->bind_param('i',$_POST['product_id']);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $product = $result->fetch_assoc();
-    if($result->num_rows == 0){
-        echo "No Product found with given Product ID";
-        exit();
-    }
-    if($_POST['quantity'] > $product['quantity']){
-        echo "Quantity requested higher than in stock";
-        exit();
-    }
 
-    $total_price = $_POST['quantity'] * $product['price'];
-}
-
-$stmt = $con->prepare('INSERT INTO orders(name,quantity,price) VALUES(?,?,?)');
+$stmt = $con->prepare('INSERT INTO meds(name,quantity,price) VALUES(?,?,?)');
 if ($stmt) {
-    $stmt->bind_param('sii',$_POST['product_name'],$_POST['quantity'],$_POST['price'],$total_price);
+    $stmt->bind_param('sii',$_POST['product_name'],$_POST['quantity'],$_POST['price']);
     $stmt->execute();
     if($con->affected_rows === 0){
         echo "Order Unsucceful, Please try again!";
     }
-    header('Location: view_order.php');
+    if($con->errno == 1062){
+        echo "Product with same name already exists";
+    }
+    else{ header('Location: inventory.php'); }
     $stmt->close();
 } else {
     echo $con->error;
